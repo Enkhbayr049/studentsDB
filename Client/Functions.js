d12d7619d9806d2fcd43318fe1b88a2f12ada78b
Enkhbayr <enkhbayr049@gmail.com>
2024-04-02 21:22:42 +0800

    tusuliin anhnii huvilbar

---
+++ Client/Functions.js
@@ -0,0 +1,153 @@
+var cds, codA, cod;
+
+function dfDt(dt1, dt2) {
+      let df = dt1.getTime() - dt2.getTime();
+      let ds = df / (1000 * 60 * 60 * 24);
+      ds = Math.floor(ds);
+      return ds;
+}
+
+function adDs(dt, nd) {
+      let ms = dt.getTime() + (nd * 24 * 60 * 60 * 1000);
+      let dt1 = new Date(ms);
+      let dt2 = sDt(dt1);
+      return dt2;
+}
+
+function sDt(dt) {
+      let y = dt.getFullYear(), m =  + dt.getMonth() + 1, d = dt.getDate();
+      let fd = y + "/" + m + "/" + d;
+      return fd;
+}
+
+function fRound(val, dec) {
+      let valFd = Number(val), dc = Number(dec);
+      valFd = valFd.toFixed(dc);
+      return valFd;
+}
+
+
+//
+
+
+
+
+
+
+async function swClasses() {  
+      await fetch("http://localhost:8822/Cls", {
+            method: "POST", headers: { "Content-Type": "application/json" }
+      })
+      .then(response => response.json())
+      .then(result => {
+            if (1 <= result.length)
+            {
+                  var ul1 = document.getElementById("uls1");
+                  for (let i = 0; i < result.length; i++) {
+                        var op = document.createElement("option");
+                        op.value = result[i].CodeA; 
+                        op.text = result[i].Name;
+                        ul1.add(op);          
+                  }
+            }
+      })
+      .catch((error) => {
+            document.getElementById("msg").value = error;
+      });
+}
+
+async function swNames(val) {
+
+      codA = val;
+      await fetch("http://localhost:8822/Nms", {
+            method: "POST", headers: { "Content-Type": "application/json" },
+            body: JSON.stringify( { cls : val } )
+      })
+      .then(response => response.json())
+      .then(result => {
+            
+            if(result.length >= 1) {
+                        cds = [];
+                        var table = document.getElementById('tbl');
+                        const tableData = 
+                        result.map(value => {
+                        cds.push(value.Code);
+                              return (
+                              `<tr onclick="swAdr(this)">
+                                    <td>${value.Name}</td>   
+                               </tr>`      
+                        );
+                        }).join('');
+                        
+                        table.innerHTML = tableData;
+                  }
+      })
+      .catch((error) => {
+            document.getElementById("msg").value = error;
+      });
+}
+
+function swAdr(obj) {
+      document.getElementById("msg").value = "";
+      // alert(obj.innerText);
+      cod = cds[obj.rowIndex];
+      fetch("http://localhost:8822/Fnd", {
+            method: "POST", headers: { "Content-Type": "application/json" },
+            body: JSON.stringify( { std : cod } )
+      })
+      .then(response => response.json())
+      .then(result => {
+            if (1 <= result.length)
+            {
+                  document.getElementById("snm").value = result[0].Name;
+                  document.getElementById("ads").value = result[0].Address;
+                  // alert(result[0].Address);
+            }
+      })
+      .catch((error) => {
+            document.getElementById("msg").value = error;
+      });
+}
+
+function fnUpDt() {
+      var ners = document.getElementById("snm").value;
+      var ads1 = document.getElementById("ads").value;
+      fetch("http://localhost:8822/UpDt", {
+            method: "POST", headers: { "Content-Type": "application/json" },
+            body: JSON.stringify( { cd : cod, ner : ners, ads : ads1 } )
+      })
+      .then(response => response.json())
+      .catch((error) => {
+            document.getElementById("msg").value = error; return;
+      });
+      document.getElementById("msg").value = 'zasav';
+}
+
+function fnIns() {
+      var cd = Math.max(...cds) + 1;
+      alert(cd);
+      var ners = document.getElementById("snm").value;
+      var ads1 = document.getElementById("ads").value;
+      fetch("http://localhost:8822/Ins", {
+            method: "POST", headers: { "Content-Type": "application/json" },
+            body: JSON.stringify( { cd : cd, ner : ners, ads : ads1 } )
+      })
+      .then(response => response.json())
+      .catch((error) => {
+            document.getElementById("msg").value = error; return;
+      });
+      document.getElementById("msg").value = 'nemev';
+}
+
+// table.innerHTML = "";
+// if (1 <= result.length)
+// {
+//       const tableData = 
+//       result.map(value => {
+//             `<tr onclick="swAds(this)">
+//                   <td>${value.Name}</td> 
+//                   <td>${value.Name}</td>       
+//                   </tr>`             
+//       }).join('');
+//       table.innerHTML = tableData;                
+// }
---
+++ Server/Index.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const app = express();
+const mysql = require('mysql2');
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234asdf",
+  database: "STDB"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+    // UPDATE Students SET CodeA = 1 WHERE Code >= 3 UPDATE Students SET CodeA = 2 WHERE Code > 3
+    // con.query("CREATE Table Marks (Code int, CodeA int, CodeL int, Mark Decimal(7,2))", function (err, result) { });
+    // con.query("UPDATE Students SET CodeA = 1 WHERE Code = 3", function (err, result) { 
+    //   if (err) throw err;
+    //   console.log("updated");
+    // });
+});
+
+app.post('/Cls', (req, res) => {
+  con.query("SELECT * FROM Classes", function (err, result) { res.send(result); });
+});
+
+app.post('/Nms', (req, res) => {
+  var cd = req.body.cls;
+  con.query("SELECT * FROM Students WHERE CodeA =" + cd, function (err, result) { res.send(result); });
+});
+
+app.post('/Fnd', (req, res) => {
+  var cd = req.body.std;
+  con.query("SELECT * FROM Students WHERE Code =" + cd, function (err, result) { res.send(result); });
+});
+
+
+app.post('/UpDt', (req, res) => {
+  var cd = req.body.cd;
+  var ner = req.body.ner;
+  var ads = req.body.ads;
+  con.query("UPDATE Students SET Name='" + ner + "', Address='" + ads + "' WHERE Code =" + cd,
+  function (err, result) { res.send(result); });
+});
+
+app.post('/Ins', (req, res) => {
+  var cd = req.body.cd;
+  var ner = req.body.ner;
+  var ads = req.body.ads;
+  var cdA = req.body.cdA;
+  con.query("INSERT INTO Students (Code, Name, Address, CodeA)" +
+  " VALUES (" + cd + " , '" + ner + "' , '" + ads + "' , " + cdA + ")" ,
+  function (err, result) { res.send(result); });
+});
+
+app.listen("8822", () => {
+   console.log("Port 8822")
+})
+
+    // con.query("SELECT * FROM Students", function (err, res) { console.log(res); });
+    // var sql = "INSERT INTO Students VALUES ?";
+    // var values = [[ 2, 'John', 'Highway 71', 1], 
+    //   [3, 'Peter', 'Lowstreet 4', 1],
+    //   [4, 'Amy', 'Apple st 652', 2],
+    //   [5, 'Hannah', 'Mountain 21', 2]];
+    // con.query(sql, [values], function (err, result) { console.log(result); })
+    
+// var mysql = require('mysql2');
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "1234asdf",
+//   database: "STDB"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   con.query("SELECT * FROM Students", function (err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
+
+//last version
+
+// var mysql = require('mysql2');
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "1234asdf",
+//   database: "STDB"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM Classes", function (err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
